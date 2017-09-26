c123a0279f15406fe934b0928db2c27e2b6c5466
Bryce Johnston <johnstonbrc@gmail.com>
2017-09-26 08:10:36 -0500

    Force SSL on prod env

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,6 @@ budget_report = ReportType.create(name: 'Budget', description: 'Budget Report')
 inventory_report = ReportType.create(name: 'Inventory', description: 'Inventory Report')
 revenue_report = ReportType.create(name: 'Revenue', description: 'Revenue Report')
 
-ReportTypeOption.create([
-  { name: 'Rent', report_type_id: budget_report.id },
-  { name: 'Utilities', report_type_id: budget_report.id },
-  { name: 'Equipment', report_type_id: budget_report.id },
-  { name: 'Computers', report_type_id: inventory_report.id },
-  { name: 'Phones', report_type_id: inventory_report.id },
-  { name: 'Vehicles', report_type_id: inventory_report.id },
-  { name: 'Consulting', report_type_id: revenue_report.id },
-  { name: 'Sales', report_type_id: revenue_report.id },
-  { name: 'Other', report_type_id: revenue_report.id }
-])
-
 User.create(
   email: 'admin@example.com',
   password: 'adminpass',
